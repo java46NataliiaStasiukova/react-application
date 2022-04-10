6f187bd0ea865e472c3d7642ad72e12b044ac28b
Nataliia <nstasiukova@gmail.com>
2022-04-10 19:28:08 +0300

    Homework-35

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
-
 import './App.css';
+import Color from './components/Color';
+import InputData from './components/InputData';
 import Timer from './components/Timer';
+import timeZones from './config/time-zones';
+import colorNames from './config/color-names';
+
+type ComponentsNames = "input-time" | "timer" | "input-color" | "color" 
 function App() {
-
-  const tmp: any = [
-   {timeZone: ''},
-   {timeZone: 'Europe/Amsterdam'},
-   {timeZone: 'australia/Sydney'},
-   {timeZone: 'America/New_York'}
-  ];
-  return <div className ="container">
-    <div className="row" style={{marginTop: "10vw"}}>
-  <Timer/>
-  <Timer interval={2000} timeZoneName={tmp[1]} zoneName={'Amsterdam'}/>
-  </div><div className="row" style={{marginTop: "10vw"}}>
-  <Timer interval={2000} timeZoneName={tmp[2]} zoneName={'Sydney'}/>
-  <Timer interval={2000} timeZoneName={tmp[3]} zoneName={'New York'}/>
-  </div></div>
-
+  const [timeZone, setTimeZone] = React.useState('Asia/Jerusalem');
+  const [color, setColor] = React.useState('red');
+  const[componentName, setComponentName] = React.useState<ComponentsNames>('timer');
+  const mapComponents: Map<ComponentsNames, React.ReactNode> = new Map;
+  mapComponents.set("input-color", <InputData data={colorNames} injectData={setColor}></InputData>)
+  mapComponents.set("color", <Color color={color}></Color>);
+  mapComponents.set("input-time", <InputData data={timeZones} injectData={setTimeZone}></InputData>)
+  mapComponents.set("timer", <Timer timeZone={timeZone}></Timer>)
+  return <div className="d-grid gap-2 col-6 mx-auto">
+    {Array.from(mapComponents.keys()).map(k => <div className="row mt-2"><button type="button" className="btn btn-info btn-lg" onClick={() => setComponentName(k)}>{k}</button></div>)}
+    {mapComponents.get(componentName)}
+  </div>
 }
 export default App;
+
+
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,21 +1,26 @@
 import React, { useEffect } from 'react';
 interface Props {
-    timeZoneName?: {}
+    timeZone?: string
     interval?: number
-    zoneName?: string
+    //zoneName?: string
 }
-const Timer: React.FC<Props>= ({timeZoneName, interval, zoneName}) => {
+const Timer: React.FC<Props>= ({timeZone, interval}) => {
     const [time, setTime] = React.useState(new Date());
     function tic(): void{
+        console.log('kuku')
         setTime(new Date());
     }
     useEffect(() => {
-        setInterval(tic, interval || 1000);
-    }, []);
-    if(zoneName === undefined) zoneName = 'Local time'
+        console.log('timer has been mounted')
+        const intervalId = setInterval(tic, interval || 1000);
+        return () => {
+            clearInterval(intervalId);
+            console.log("unmounting timer")
+    }}, [interval]);
+    //if(zoneName === undefined) zoneName = 'Local time'
     return <div className="col" style={{marginLeft: "10vw"}}>
-        <h3>{zoneName}</h3>
-        <label>{time.toLocaleTimeString('en-US', timeZoneName || undefined)}</label>
+        <h3>{timeZone}</h3>
+        <label>{time.toLocaleTimeString('en-US', {timeZone})}</label>
     </div>
 }
 export default Timer;
